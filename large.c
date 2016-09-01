90898ea83e0021d31d9d8d8d18f1d8bd1243e712
pradeepashanmugam <pradeepakshanmugam@gmail.com>
2016-09-01 14:26:30 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,z;
+if(x>y&&x>z)
+printf("enter the 1st number");
+elseif(y>x&&y>z)
+printf("enter the 2nd number");
+elseif(z>x&&z>y)
+printf("enter the 3rd number");
+else
+printf("enter the number is not");
+}
