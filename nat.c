b80cf5807445e4cee7c40875c5b30928095c0164
pradeepashanmugam <pradeepakshanmugam@gmail.com>
2016-09-01 15:00:12 +0530

    Create nat.c

---
+++ nat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=1,n,sum=0;
+printf("enter the number");
+scanf("%d",&n);
+while(i<=n)
+sum+=i;
+++i;
+printf("sum");
+return 0;
+getch()
+}
