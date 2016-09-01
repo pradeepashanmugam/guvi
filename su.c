0cb667f46562a0a63ef86ed965742bee46210f43
pradeepashanmugam <pradeepakshanmugam@gmail.com>
2016-09-01 15:07:56 +0530

    Create su.c

---
+++ su.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n,sum=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+sum=sum+i;
+printf("sum of natural number");
+return 0;
+}
