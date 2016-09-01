e367ec7cf6c5c70747a49c9edfd6dca436d256ad
pradeepashanmugam <pradeepakshanmugam@gmail.com>
2016-09-01 15:29:52 +0530

    Create squ.c

---
+++ squ.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int base,exponent;
+long long power=1;
+int i;
+printf("enter the base");
+scanf("%d",&n);
+printf("enter the exponent");
+scnanf("%d",&n);
+for(i=1;i<=exponent;i++)
+pow=pow*base;
+printf("base,exponent");
+return 0;
+getch();
+}
