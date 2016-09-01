890cb1143a2825b943d46153aece2bfc7ebf344f
pradeepashanmugam <pradeepakshanmugam@gmail.com>
2016-09-01 15:16:06 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num,count=0;
+printf("enter the num");
+scanf("%d",&n);
+while(num)
+num=num/10;
+count++;
+printf("count=");
+return 0;
+getch();
+}
